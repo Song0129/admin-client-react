78677b8d357d40578c026286264b20596e7f92cf
Song0129 <Song_3191@163.com>
2021-08-30 16:35:49 +0800

    update

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@
  * 应用的根组件
  */
 import React, { Component } from 'react';
-import { Button } from 'antd';
+import { Button, message } from 'antd';
 
 export default class App extends Component {
+	handleClick = () => {
+		message.success('success');
+	};
 	render() {
 		return (
 			<div>
-				App.. <Button type='primary'>123</Button>
+				App..{' '}
+				<Button type='primary' onClick={this.handleClick}>
+					123
+				</Button>
 			</div>
 		);
 	}
