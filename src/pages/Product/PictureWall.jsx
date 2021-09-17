413290ff619f72e614bd53f9a0a2f115d4df5121
Song0129 <Song_3191@163.com>
2021-09-17 20:43:38 +0800

    update

--- src/pages/Product/PictureWall.jsx
+++ src/pages/Product/PictureWall.jsx
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 
 export default class PictureWall extends Component {
+	state = {
+		previewVisible: false, // 标识是否显示大图预览Modal
+		previewImage: '', // 大图的url
+		fileList: [],
+	};
 	render() {
 		return <div></div>;
 	}
