16ec35275bfb1e347b5a853b643f6d4271e9471d
Song0129 <Song_3191@163.com>
2021-09-18 15:30:43 +0800

    update

--- src/api/index.js
+++ src/api/index.js
@@ -1,17 +1,43 @@
 // import { message } from 'antd';
 import axios from './axios';
-import { users, category } from '../config/urls'; //引入地址
+import { userUrls, categoryUrls, productUrls } from '../config/urls'; //引入地址
 import jsonp from 'jsonp';
 
 // 登录
-export const reqLogin = (username, password) => axios.post(users.login, { username, password });
+export const reqLogin = (username, password) => axios.post(userUrls.login, { username, password });
 
 // 获取一级或某个二级分类列表
-export const reqCategorys = parentId => axios.get(category.list, { parentId });
+export const reqCategorys = parentId => axios.get(categoryUrls.list, { parentId });
 // 添加分类
-export const reqAddCategory = (parentId, categoryName) => axios.post(category.add, { parentId, categoryName });
+export const reqAddCategory = (parentId, categoryName) => axios.post(categoryUrls.add, { parentId, categoryName });
 // 更新品类名称
-export const reqUpdateCategory = (categoryId, categoryName) => axios.post(category.update, { categoryId, categoryName });
+export const reqUpdateCategory = (categoryId, categoryName) => axios.post(categoryUrls.update, { categoryId, categoryName });
+
+// 获取一个分类
+export const reqCategory = categoryId => axios.get(categoryUrls.info, { categoryId });
+
+// 获取商品分页列表
+export const reqProducts = (pageNum, pageSize) => axios.get(productUrls.list, { pageNum, pageSize });
+
+// 更新商品状态(上架/下架)
+export const reqUpdateStatus = (productId, status) => axios.post(productUrls.updateStatus, { productId, status });
+
+/*
+    搜索商品分页列表(根据商品名称和商品描述)
+    searchType:搜索的类型, productName/productDesc
+*/
+export const reqSearchProducts = ({ pageNum, pageSize, searchName, searchType }) =>
+	axios.get(productUrls.search, {
+		pageNum,
+		pageSize,
+		[searchType]: searchName,
+	});
+
+// 删除指定名称的图片
+export const reqDeleteImg = name => axios.post(productUrls.deleteImg, { name });
+
+// 添加/修改商品
+export const reqAddOrUpdateProduct = product => axios.post(product._id ? productUrls.update : productUrls.add, product);
 
 // 获取百度天气
 export const reqWeather = city => {
--- src/config/urls.js
+++ src/config/urls.js
@@ -2,18 +2,24 @@
  * 统一保存请求地址
  */
 // 用户相关
-export const users = {
+export const userUrls = {
 	login: '/login',
 };
 
 // 分类相关
-export const category = {
+export const categoryUrls = {
 	list: '/manage/category/list',
 	add: '/manage/category/add',
 	update: '/manage/category/update',
+	info: '/manage/category/info',
 };
 
 // 商品相关
-export const product = {
-	list: 'mange',
+export const productUrls = {
+	list: '/manage/product/list',
+	updateStatus: '/manage/product/updateStatus',
+	search: '/manage/product/search',
+	deleteImg: '/manage/img/delete',
+	add: '/manage/product/add',
+	update: '/manage/product/update',
 };
