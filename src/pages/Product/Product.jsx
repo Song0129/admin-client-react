6de55f8b85f007225b73acba46ecc3c15679fae8
Song0129 <Song_3191@163.com>
2021-09-18 10:58:39 +0800

    update

--- src/pages/Product/Product.jsx
+++ src/pages/Product/Product.jsx
@@ -1,7 +1,19 @@
 import React, { Component } from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
+
+import ProductHome from './Home';
+import RroductAdd from './Add';
+import ProductDetail from './Detail';
 
 export default class Product extends Component {
 	render() {
-		return <div>Product....</div>;
+		return (
+			<Switch>
+				<Route path='/products/product' component={ProductHome} exact />
+				<Route path='/products/product/add' component={RroductAdd} />
+				<Route path='/products/product/detail' component={ProductDetail} />
+				<Redirect to='/products/product' />
+			</Switch>
+		);
 	}
 }
